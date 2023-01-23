8b2896bc16f65ae9e23d518639dc3a92f883ce1b
Paul DeLeeuw <pauldl+gh@gmail.com>
2023-01-22 21:25:45 -0500

    Adding flask server

---
+++ calpost.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/post', methods = ['POST'])
+def post():
+  try:
+    up_data = json.loads(request.data)
+    events = open('events.json', 'w')
+    json.dump(up_data, events)
+    return 'ok'
+  except json.JSONDecodeError:
+    return 'malformed'
+
+app.run(host='0.0.0.0', port=8090)
+
